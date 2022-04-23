307d70726524abe2dd484f8df5ae0e8ada829753
JNielacny <naczelnyh8r@gmail.com>
2022-04-23 17:21:55 +0200

    ta duża kropa w sensie podpunkt drugi dodalem wszystko co wskazane w tym podpunkcie

--- z4-zalazek/src/Wektor.cpp
+++ z4-zalazek/src/Wektor.cpp
@@ -7,3 +7,79 @@
  *  niz dwie linijki.
  *  Mniejsze metody mozna definiwac w ciele klasy.
  */
+
+    /*
+  double &Wektor::set_Tab(int x)
+  {
+      return Tab[x];
+  }
+  double Wektor::get_Tab(int x)
+  {
+      return Tab[x];
+  } */
+
+  double &Wektor::operator [](int x)
+  {
+      if(x<0 || x>=ROZMIAR)
+      {
+          cerr<<"Blad: Zly indeks"<<endl;
+          return Tab[x];
+      }
+      else
+      return Tab[x];
+  }
+
+  double Wektor::operator [](int x)
+  {
+      if(x<0 || x>=ROZMIAR)
+      {
+          cerr<<"Blad: Zly indeks"<<endl;
+          return Tab[x];
+      }
+      else
+      return Tab[x];
+  }
+
+  Wektor Wektor::operator -(Wektor Wart)
+  {
+      Wektor Roznica;
+      for(int x=0; x<ROZMIAR; x++)
+        Roznica[x] = Tab[x] - Wart[x];
+      return Roznica; 
+  }
+
+  Wektor Wektor::operator *(double Wart)
+  {
+      Wektor Iloczyn;
+      for(int x=0; x<ROZMIAR; x++)
+        Iloczyn[x] = Tab[x] * Wart;
+      return Iloczyn;
+  }
+
+  Wektor Wektor::operator /(double Wart)
+  {
+      Wektor Iloraz;
+      for(int x=0; x<ROZMIAR; x++)
+        Iloraz[x] = Tab[x] / Wart;
+      return Iloraz;
+  }
+
+  double Wektor::operator *(Wektor Wart)
+  {
+      Wektor Skalar;
+      for(int x=0; x<ROZMIAR; x++)
+        Skalar += Tab[x] * Wart[x];
+      return Skalar;
+  }
+
+  std::istream &operator >> (std::istream &Strm, Wektor &Wek)
+  {
+      for(int x=0; x<ROZMIAR; x++)
+        Strm>>Wek[x];
+  }
+
+  std::ostream &operator << (std::ostream &Strm, const Wektor &Wek)
+  {
+      for(int x=0; x<ROZMIAR; x++)
+        Strm<<Wek[x]<<" ";
+  }
